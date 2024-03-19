6f64a52a5f9e9d582a4980e89a8097a5d95ec1c7
Fer Ordoñez <111704567+fer-of@users.noreply.github.com>
2024-03-19 10:15:01 -0600

    Adición de javadocs

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"poop1","c":"POOP1","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"poop1","c":"POOP1","l":"POOP1()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop1","l":"POOP1"}];updateSearchResults();
