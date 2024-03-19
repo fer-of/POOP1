f1f48828394ef55cb68a1eb916753476ff09c6d9
Fer Ordoñez <111704567+fer-of@users.noreply.github.com>
2024-03-19 10:16:25 -0600

    Practica 1- Creación de Hola Mundo en Java - POO

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/**
+ * Creacion del paquete poop1 que tendra una clase POOP1
+ */
+
+package poop1;
+
+/**
+ *
+ * @author marif
+ * Creacion de la clase POOP1 dentro del package poop1
+ */
+public class POOP1 {
+
+    /**
+     * Este es el metodo principal
+     * 
+     * @param args argumento de consola del metodo main
+     * 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
